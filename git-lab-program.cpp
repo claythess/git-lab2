5af7a9d45cb27604928ba52ad0847b454f63c5fa
Clay Hess <ch559521@ohio.edu>
2022-09-06 17:18:18 -0400

    Add program

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Clay Hess>
+ *        Date: <9/6/22>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
